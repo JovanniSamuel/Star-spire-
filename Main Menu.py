04218fb5249635a98707b924b3a5ddfc8789e971
Jovanni Samuel <298763260+JovanniSamuel@users.noreply.github.com>
2026-07-31 16:37:13 -0400

    Enhance main menu with sound and character classes
    Added sound effects and character classes to the main menu.

--- Main Menu.py
+++ Main Menu.py
@@ -1,12 +1,73 @@
 import pygame
 
+
 pygame.init()
+pygame.mixer.init()
 
 WIDTH, HEIGHT = 1000, 600 #Size of window
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT)) #Makes window
 pygame.display.set_caption("Trinity of one")  #Names window
 clock = pygame.time.Clock() #Set the fps
+
+# Sound effects ####################################
+pygame.mixer.music.load("main_menu_sound.mp3")
+pygame.mixer.music.set_volume(0.5) #volume of sound
+pygame.mixer.music.play(-1)# ensues that it loops forever
+
+Classes 
+
+class Character(pygame.sprite.Sprite): # starts the charcter class
+    def __init__(self, name, load_sprite,  size, x, y, hp=100,attack=10, speed=10, mana=100,defense=5):
+        super().__init__()
+        self.name = name
+        self.image= load_image(load_sprite, size)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.hp = hp
+        self.attack = attack
+        self.speed = speed
+        self.max_hp = hp
+        self.mana = mana
+        self.defense = defense
+        
+    def take_damage(self, amount):
+        reduced = max(amount - self.defense, 1)
+        self.hp -= reduced
+        if self.hp < 0:
+            self.hp = 0
+        return reduced
+    
+    def is_alive(self):
+        return self.hp > 0
+    
+    def attack_target(self, target):
+        target.take_damage(self.attack)
+        
+        
+class Player(Character):
+    def __init__(self, name, load_sprite, size, x, y, hp=200, attack=10, speed=10, mana=250, defense=5,mana_regen=5):
+        super().__init__(name, load_sprite, size, x, y, hp, attack, speed, mana, defense)
+        self.name = name
+        
+class Summoner(Character):
+    def __init__(self, name, player_sprite, size, x, y, hp=200, attack=10, speed=10, mana=250, defense=5, mana_regen=6):
+        super().__init__(name, player_sprite, size, x, y, hp, attack, speed, mana, defense)
+        self.familliar = []
+        self.mana = mana
+        self.max_mana = mana
+        self.mans_regen = mana_regen
+      
+    def summon_familliar(self, familiar_group, load_sprite, size, x, y, mana_cost=30):
+        self.familliar = [f for f in self.familiars if f.is_alive()]
+        
+        if self.mana < mana_cost:
+            return None
+        
+        self.mana -= mana_cost
+        familiar = Familiar(self, familiar_type, load_sprite
+    
 
 BLACK = (0, 0, 0) #Background color
 
